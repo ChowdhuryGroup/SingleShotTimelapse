c9571db29a65030593124c7fcf666781fc0c0cdc
kuz11 <kuz.1@osu.edu>
2025-03-30 14:15:21 -0400

    Writing outline and adding data

---
+++ main.py
@@ -0,0 +1,6 @@
+# load all images to corresponding times
+# Crop all images to each channel
+# Find the edge of sample for each channel in the before image
+# set each channel to the same contrast setting? Not sure if this is the right place to do it
+# divide the before and during
+# organize the images in time
